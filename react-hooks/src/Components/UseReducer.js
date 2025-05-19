f4adbd99cc5e7d0dc1987c277485bacc96a0ef1f
Ajay <ajayshakya7376@gmail.com>
2025-05-20 03:08:34 +0530

    feat : create a infinite scroll animation project

--- react-hooks/src/Components/UseReducer.js
+++ react-hooks/src/Components/UseReducer.js
@@ -1,39 +1,93 @@
-import React,{useReducer} from 'react';
+// import React, { useReducer } from "react";
 
-const initialState=0;
-const reducer= (state,action)=>{
-    switch(action){
-        case "add":
-            return state=state+1;
-        case "substract":
-             return state=state-1;
-        case "reset":
-            return 0;
-        default:
-            throw new Error("unexpected Error");
-    }
+// const initialState = 0;
+// const reducer = (state, action) => {
+//   switch (action) {
+//     case "add":
+//       return (state = state + 1);
+//     case "substract":
+//       return (state = state - 1);
+//     case "reset":
+//       return 0;
+//     default:
+//       throw new Error("unexpected Error");
+//   }
+// };
 
+// const UseReducer = () => {
+//   const [count, dispatch] = useReducer(reducer, initialState);
+
+//   return (
+//     <div>
+//       <h2>UseEffect</h2>
+
+//       <h2>Count Is Here - {count}</h2>
+//       <button
+//         style={{
+//           backgroundColor: "red",
+//           color: "white",
+//           padding: "10px",
+//           border: "none",
+//           borderRadius: "10px",
+//           margin: "0px 10px",
+//         }}
+//         onClick={() => dispatch("add")}
+//       >
+//         Add
+//       </button>
+
+//       <button
+//         style={{
+//           backgroundColor: "red",
+//           color: "white",
+//           padding: "10px",
+//           border: "none",
+//           borderRadius: "10px",
+//           margin: "0px 10px",
+//         }}
+//         onClick={() => dispatch("substract")}
+//       >
+//         substract
+//       </button>
+//       <button
+//         style={{
+//           backgroundColor: "red",
+//           color: "white",
+//           padding: "10px",
+//           border: "none",
+//           borderRadius: "10px",
+//           margin: "0px 10px",
+//         }}
+//         onClick={() => dispatch("reset")}
+//       >
+//         reset
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default UseReducer;
+
+import React, { useReducer } from "react";
+
+const initialState = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case "add":
+      return (state = state + 1);
+    case "sub":
+      return (state = state - 1);
+    default:
+      return state;
+  }
+};
+
+export default function UseReducer() {
+    const [count , dispatch]=useReducer(reducer,initialState);
+  return <div>
+    <h1>{count}</h1>
+    <button onClick={()=>{dispatch("add")}}>add</button>
+
+    
+  </div>;
 }
-
-const UseReducer = () => {
-    const [count , dispatch]=useReducer(reducer,initialState)
-
-    return <div>
-
-
-        <h2>UseEffect</h2>
-
-        <h2>Count Is Here - {count}</h2>
-        <button style={{backgroundColor:"red",color:"white" ,padding:"10px",border:"none", borderRadius:"10px",margin:"0px 10px"}} 
-        onClick={()=>dispatch("add")}>Add</button>
-
-        <button style={{backgroundColor:"red",color:"white" ,padding:"10px",border:"none", borderRadius:"10px",margin:"0px 10px"}} onClick={()=>dispatch("substract")}>substract</button>
-        <button style={{backgroundColor:"red",color:"white" ,padding:"10px",border:"none", borderRadius:"10px" , margin:"0px 10px"}} onClick={()=>dispatch("reset")}>reset</button>
-        
-
-
-    </div>;
-}
-
-
-export default UseReducer;
--- react-hooks/src/Components/UseState.js
+++ react-hooks/src/Components/UseState.js
@@ -1,20 +1,28 @@
-import {React,useState} from 'react';
+import { React, useState } from "react";
 const UseState = () => {
-    const [click, setClick] = useState(0);
+  const [click, setClick] = useState(0);
 
-
-    return (
-        <div>
-            <p>You've clicked {click} times!</p>
-            <p>The number of times you have clicked
-            </p>  
-            <button style={{backgroundColor:"red",color:"white" ,padding:"10px",border:"none", borderRadius:"10px"}} onClick={() => setClick(click => click + 1)}>
-                Click me
-            </button>
-        </div>
-    );
-
-    
-}
+  return (
+    <div>
+      <p>You've clicked {click} times!</p>
+      <p>The number of times you have clicked</p>
+      {/* <button
+        style={{
+          backgroundColor: "red",
+          color: "white",
+          padding: "10px",
+          border: "none",
+          borderRadius: "10px",
+        }}
+        onClick={() => setClick((click) => click + 1)}
+      >
+        Click me
+      </button> */}
+      <button  onClick={() => setClick((click) => click + 1)}>
+        click
+      </button>
+    </div>
+  );
+};
 
 export default UseState;
